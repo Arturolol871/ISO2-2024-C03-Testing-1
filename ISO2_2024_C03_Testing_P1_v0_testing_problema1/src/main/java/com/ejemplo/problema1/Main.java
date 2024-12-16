5bcd5b045aa24fc19b3d5d3d7bd5094804993309
Arturolol871 <146983233+Arturolol871@users.noreply.github.com>
2024-12-16 11:13:03 +0100

    Version con fallos del testeo del problema 1
    Version la cual no estaba bien configurado jacoco ni bien definidas las pruebas unitarias

---
+++ ISO2_2024_C03_Testing_P1_v0_testing_problema1/src/main/java/com/ejemplo/problema1/Main.java
@@ -0,0 +1,233 @@
+package com.ejemplo.problema1;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		
+		 // Atributos precargados
+		 String nombre = "Juan";
+		 String apellidos = "PÃ©rez";
+		 String fechaNacimiento = "01-01-2000"; // Formato dd-mm-yyyy
+		 String nacionalidad = "EspaÃ±a";
+		 String titulacion = "Master";
+		 boolean certificacionIngles = true;  // Asumiendo que tiene certificaciÃ³n
+		 String telefono = "123456789";
+		 String correoElectronico = "juan@example.com";
+ 
+		 // Creando una persona con los atributos precargados
+		 Persona persona = new Persona(nombre, apellidos, fechaNacimiento, nacionalidad, titulacion, certificacionIngles, telefono, correoElectronico);
+ 
+
+		
+		System.out.println("");
+		if (persona.esMayorDeEdad()) {
+			System.out.println("La persona es mayor de edad.");
+		} else {
+			System.out.println("La persona NO es mayor de edad.");
+		}
+
+		if (Persona.esEuropea(persona.getNacionalidad())) {
+			System.out.println("La persona tiene nacionalidad europea.");
+		} else {
+			System.out.println("La persona NO tiene nacionalidad europea.");
+		}
+		
+		if (persona.matricularDoctorado()) {
+	        System.out.println("La persona puede matricularse en un doctorado.");
+	    } else {
+	        System.out.println("La persona NO puede matricularse en un doctorado.");
+	    }
+	}
+
+	public static Persona crearPersona(Scanner sc) {
+		String nombre = leerNombre(sc);
+		String apellidos = leerApellidos(sc);
+		String fechaNacimiento = leerFechaNacimiento(sc);
+		String nacionalidad = leerNacionalidad(sc);
+		String titulacion = leerTitulacion(sc);
+		boolean certificacionIngles = leerCertificacionIngles(sc);
+		String telefono = leerTelefono(sc);
+		String correoElectronico = leerCorreoElectronico(sc);
+
+		Persona persona = new Persona(nombre, apellidos, fechaNacimiento, nacionalidad, titulacion, certificacionIngles,
+				telefono, correoElectronico);
+
+		return persona;
+	}
+
+	public static String leerNombre(Scanner sc) {
+		String nombre;
+		do {
+			System.out.print("Introduce el nombre: ");
+			nombre = sc.nextLine().trim();
+
+			if (nombre.isEmpty()) {
+				System.out.println("El nombre no puede estar vacï¿½o.");
+			} else if (nombre.matches(".*\\d.*")) {
+				System.out.println("El nombre no puede contener nï¿½meros.");
+			} else {
+				break;
+			}
+
+		} while (true);
+
+		return nombre;
+	}
+
+	public static String leerApellidos(Scanner sc) {
+		String apellidos;
+		do {
+			System.out.print("Introduce los apellidos: ");
+			apellidos = sc.nextLine().trim();
+
+			if (apellidos.isEmpty()) {
+				System.out.println("Los apellidos no pueden estar vacï¿½os.");
+			} else if (apellidos.matches(".*\\d.*")) {
+				System.out.println("Los apellidos no pueden contener nï¿½meros.");
+			} else {
+				break;
+			}
+
+		} while (true);
+
+		return apellidos;
+	}
+
+	public static String leerFechaNacimiento(Scanner sc) {
+		String fechaNacimiento;
+		do {
+			System.out.print("Introduce la fecha de nacimiento (DD-MM-YYYY): ");
+			fechaNacimiento =  sc.nextLine().trim();
+
+			// formato de la fecha DD-MM-YYYY
+			if (!fechaNacimiento.matches("\\d{2}-\\d{2}-\\d{4}")) {
+				System.out.println("El formato de la fecha debe ser DD-MM-YYYY.");
+			} else {
+				String[] partes = fechaNacimiento.split("-");
+				int dia = Integer.parseInt(partes[0]);
+				int mes = Integer.parseInt(partes[1]);
+				int anio = Integer.parseInt(partes[2]);
+
+				if (dia < 1 || dia > 31 || mes < 1 || mes > 12 || anio < 1900 || anio > 2024) {
+					System.out.println("La fecha introducida no es vï¿½lida.");
+				} else {
+					// Verificar si la fecha no es futura
+					if (anio == 2024 && (mes > 12 || (mes == 12 && dia > 31))) {
+						System.out.println("La fecha de nacimiento no puede ser futura.");
+					} else {
+						break;
+					}
+				}
+			}
+
+		} while (true);
+
+		return fechaNacimiento;
+	}
+
+	public static String leerNacionalidad(Scanner sc) {
+		String nacionalidad;
+		do {
+			System.out.print("Introduce la nacionalidad: ");
+			nacionalidad =  sc.nextLine().trim();
+
+			if (nacionalidad.isEmpty()) {
+				System.out.println("La nacionalidad no puede estar vacï¿½a.");
+			} else {
+				break;
+			}
+
+		} while (true);
+
+		return nacionalidad;
+	}
+
+	public static String leerTitulacion(Scanner sc) {
+		String titulacion;
+		String[] titulacionesValidas = { "Grado", "Master", "Doctorado" };
+		boolean esValida = false;
+		do {
+			System.out.print(
+					"Introduce la titulaciï¿½n (Grado, Master, Doctorado): ");
+			titulacion =  sc.nextLine().trim();
+
+			for (String titulo : titulacionesValidas) {
+				if (titulacion.equalsIgnoreCase(titulo)) {
+					esValida = true;
+					break;
+				}
+			}
+
+			if (!esValida) {
+				System.out.println(
+						"La titulaciï¿½n introducida no es vï¿½lida. Debe ser una de las siguientes: Primaria, Secundaria, Bachillerato, Grado, Master, Doctorado.");
+			}
+
+		} while (titulacion == null || titulacion.isEmpty() || esValida == false);
+
+		return titulacion;
+	}
+
+	public static boolean leerCertificacionIngles(Scanner sc) {
+	    String respuesta;
+	    boolean tieneCertificacion = false;
+
+	    while (true) {
+	        System.out.print("¿Tiene certificación de inglés? (si/no): ");
+	        respuesta = sc.nextLine().trim().toLowerCase();
+
+	        switch (respuesta) {
+	            case "si":
+	                tieneCertificacion = true;
+	                return tieneCertificacion;
+
+	            case "no":
+	                tieneCertificacion = false;
+	                return tieneCertificacion;
+
+	            default:
+	                System.out.println("Por favor, introduce una respuesta válida (si/no).");
+	        }
+	    }
+	}
+
+
+	public static String leerTelefono(Scanner sc) {
+		String telefono;
+		do {
+			System.out.print("Introduce el nï¿½mero de telï¿½fono (9 dï¿½gitos): ");
+			telefono =  sc.nextLine().trim();
+
+			// Verificar que el telï¿½fono tenga 9 dï¿½gitos y que contenga solo nï¿½meros
+			if (telefono.length() != 9) {
+				System.out.println("El nï¿½mero de telï¿½fono debe tener 9 dï¿½gitos.");
+			} else if (!telefono.matches("\\d{9}")) {
+				System.out.println("El nï¿½mero de telï¿½fono debe contener solo nï¿½meros.");
+			} else {
+				break;
+			}
+		} while (true);
+
+		return telefono;
+	}
+
+	public static String leerCorreoElectronico(Scanner sc) {
+		String correoElectronico;
+		do {
+			System.out.print("Introduce el correo electrï¿½nico: ");
+			correoElectronico =  sc.nextLine().trim();
+
+			// Verificar que el correo tenga un formato vï¿½lido
+			if (!correoElectronico.matches("^[^@]+@[^@]+\\.[a-zA-Z]{2,}$")) {
+				System.out.println("El correo electrï¿½nico no es vï¿½lido.");
+			} else {
+				break;
+			}
+		} while (true);
+
+		return correoElectronico;
+	}
+
+}
